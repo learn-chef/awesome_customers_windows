e859f19bdc0316a2361aa517b9511219fd790743
tpetchel <tpetchel@gmail.com>
2016-02-12 18:14:29 -0500

    Bump sql_server version

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,5 @@ description 'Installs/Configures awesome_customers_windows'
 long_description 'Installs/Configures awesome_customers_windows'
 version '0.1.0'
 
-depends 'sql_server', '~> 2.4.0'
+depends 'sql_server', '~> 2.5.0'
 depends 'iis', '~> 4.1.6'
