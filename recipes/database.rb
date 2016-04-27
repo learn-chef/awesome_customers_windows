9ca094e6fee89bd1359b19cb84237d02368a5fed
tpetchel <tpetchel@gmail.com>
2016-04-27 15:14:43 -0400

    Remove data bags, simplify .kitchen.ec2.yml
    + Replace use of data bags with node attributes.
    + Simply .kitchen.ec2.yml

--- recipes/database.rb
+++ recipes/database.rb
@@ -3,13 +3,6 @@
 # Recipe:: database
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
-# Load the secrets file and the encrypted data bag item that holds the sa password.
-password_secret = Chef::EncryptedDataBagItem.load_secret(node['awesome_customers_windows']['secret_file'])
-password_data_bag_item = Chef::EncryptedDataBagItem.load('database_passwords', 'sql_server_customers', password_secret)
-
-# Set the node attribute that holds the sa password with the decrypted passoword.
-node.default['sql_server']['server_sa_password'] = password_data_bag_item['sa_password']
-
 # Install SQL Server.
 include_recipe 'sql_server::server'
 
